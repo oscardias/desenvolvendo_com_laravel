3551e12c7bd42d7428e439514162ea815159649e
Oscar Dias <oscardias@gmail.com>
2014-03-04 20:15:10 -0300

    Modificações do artigo Iniciando um Novo Projeto no Laravel

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'http://desenvolvendo_com_laravel.localhost',
 
 	/*
 	|--------------------------------------------------------------------------
